27196456cdbddb0ccf6f477f11bbd41bb0ba009e
NikkiE-Holberton <2495@holbertonschool.com>
2021-06-21 15:32:52 +0000

    adding replae method to code

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def display():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def cfile(text):
+    return 'C {}'.format(text.replace("_", " "))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
