b77955397e2a77077431823fba6436fced99d926
NikkiE-Holberton <2495@holbertonschool.com>
2021-06-21 16:26:13 +0000

    Adding code for input in html

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def display():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def cfile(text):
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def pyfile(text="is cool"):
+    return 'Python {}'.format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>')
+def nmbfile(n):
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def nmbhtml(n):
+    return render_template('5-number.html', value=n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
