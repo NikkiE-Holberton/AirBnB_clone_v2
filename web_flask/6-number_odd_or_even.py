d73580b1c585f3a02b41a0009b443fe1505f8b5d
NikkiE-Holberton <2495@holbertonschool.com>
2021-06-21 18:51:24 +0000

    Adding even/odd condition

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def display():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def cfile(text):
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def pyfile(text="is cool"):
+    return 'Python {}'.format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>')
+def nmbfile(n):
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def nmbhtml(n):
+    return render_template('5-number.html', value=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def nmbhtml(n):
+    return render_template('6-number_odd_or_even.html', value=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
