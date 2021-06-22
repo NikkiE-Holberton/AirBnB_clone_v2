fdf4aa99b787d49c553bfaee3db53e117604bd4f
NikkiE-Holberton <2495@holbertonschool.com>
2021-06-22 22:36:33 +0000

    Updating close & adding import

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,7 +5,8 @@ from models.engine.file_storage import FileStorage
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
 from os import getenv
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker, Session
+from sqlalchemy.org.session import Session
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
@@ -88,4 +89,4 @@ class DBStorage():
         self.__session = Session()
 
     def close(self):
-        Session.close()
+        self.__session.close()
